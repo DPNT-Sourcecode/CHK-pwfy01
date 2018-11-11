de6c7bb0304e324ee970fe473c6ab69b79460124
sbx_user1058 <sbx_user1058@ip-10-128-156-238.eu-west-2.compute.internal>
2018-11-11 21:31:40 +0000

    Sun Nov 11 21:31:40 UTC 2018

---
+++ lib/solutions/CHK/test_offer.py
@@ -0,0 +1,29 @@
+import unittest
+
+from checkout_solution import Offer
+
+
+class TestOffer(unittest.TestCase):
+
+    def setUp(self):
+        self.offers_list = [
+            {
+                "offer_id": 1,
+                "quantity": 3,
+                "price": 130
+            },
+            {
+                "offer_id": 2,
+                "quantity": 2,
+                "price": 45
+            }
+        ]
+
+    def test_offer_is_available(self):
+        offer = Offer(1)
+        self.assertTrue(offer.is_available())
+
+    def test_offer_not_available(self):
+        offer = Offer(100)
+        self.assertFalse(offer.is_available())
+
