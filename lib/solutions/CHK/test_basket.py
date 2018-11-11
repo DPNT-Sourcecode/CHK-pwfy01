431a5548169a676a26e6ae5d93c350bccf755abe
sbx_user1058 <sbx_user1058@ip-10-128-156-238.eu-west-2.compute.internal>
2018-11-11 21:37:40 +0000

    Sun Nov 11 21:37:40 UTC 2018

---
+++ lib/solutions/CHK/test_basket.py
@@ -0,0 +1,26 @@
+import unittest
+
+from checkout_solution import Basket
+
+
+class TestBasket(unittest.TestCase):
+
+    def setUp(self):
+        self.valid_basket_1 = u'AAbC'
+        self.valid_basket_2 = u''
+        self.not_valid_basket_1 = u'%B1'
+        self.not_valid_basket_2 = u'a'
+
+    def test_is_valid_basket(self):
+        basket1 = Basket(self.valid_basket_1)
+        self.assertTrue(basket1.is_valid())
+
+        basket2 = Basket(self.valid_basket_2)
+        self.assertTrue(basket2.is_valid())
+
+    def test_is_not_valid_basket(self):
+        basket1 = Basket(self.not_valid_basket_1)
+        self.assertFalse(basket1.is_valid())
+
+        basket2 = Basket(self.not_valid_basket_2)
+        self.assertFalse(basket2.is_valid())
